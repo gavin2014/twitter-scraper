a36514de1425cf6c949663b02a1b4c01079e4622
大盖文 <812568591@qq.com>
2024-08-08 11:20:44 +0800

    Update search_test.go

--- search_test.go
+++ search_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"testing"
 
-	twitterscraper "github.com/imperatrona/twitter-scraper"
+	twitterscraper "github.com/gavin2014/twitter-scraper"
 )
 
 func TestFetchSearchCursor(t *testing.T) {
